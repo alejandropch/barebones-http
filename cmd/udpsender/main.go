ca36355abd6f1b943e61e38db06879e26e8a59ec
Alejandro Pachas <alejandropachas1@gmail.com>
2025-09-29 17:49:59 -0500

    udp connection created. Runs in port 1234 and sends data to port 4321

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	lAddr, err := net.ResolveUDPAddr("udp", ":1234")
+	check(err)
+	rAddr, err := net.ResolveUDPAddr("udp", ":4321")
+	check(err)
+	conn, err := net.DialUDP("udp", lAddr, rAddr)
+	defer conn.Close()
+	reader := bufio.NewReader(os.Stdin)
+	check(err)
+	for {
+		fmt.Printf(">")
+		str, err := reader.ReadString('\n')
+		check(err)
+		_, err = conn.Write([]byte(str))
+		check(err)
+	}
+
+}
